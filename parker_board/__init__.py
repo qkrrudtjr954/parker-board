f43ae4408a9858b37c4cea8966a7e8703883f6cf
Parker <qkrrudtjr954@kangwon.ac.kr>
2018-05-25 19:27:15 +0900

    add Flask-Session

--- parker_board/__init__.py
+++ parker_board/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
-
+from flask.ext.session import Session
 
 def create_app(config=None):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/parkerdev'
     app.config['SECRET_KEY'] = 'dev'
+
+    Session(app)
 
     from parker_board import model as db
     db.init_app(app)
