e4bba036b531e2ae4a5f433b3d34a0d63c7222e6
Parker <qkrrudtjr954@kangwon.ac.kr>
2018-06-12 15:05:16 +0900

    post schema

--- app/schema/post.py
+++ app/schema/post.py
@@ -1,11 +1,61 @@
-from marshmallow import fields, post_dump
+from marshmallow import fields
 from marshmallow_enum import EnumField
 from app.model import db
-from app.model.comment import Comment, CommentStatus
+from app.model.comment import CommentStatus
 from app.schema import ma
 from app.model.post import Post, PostStatus
 from app.schema.user import simple_user_schema
 from app.schema.board import simple_board_schema
+
+
+'''
+스키마는 입력값 정제 vs 나가는값 정제
+
+입력값 정제 : 필요한 스키마 만들어서 사용
+출력값 정제 : 기본 스키마에서 only를 사용
+'''
+
+
+class PostSchema(ma.ModelSchema):
+    board = fields.Nested(simple_board_schema)
+    user = fields.Nested(simple_user_schema)
+
+    comments_count = fields.Method('get_comments_count')
+
+    status = EnumField(PostStatus)
+
+    def get_comments_count(self, obj):
+        comments = [c for c in obj.comments if c.status != CommentStatus.DELETED]
+        return len(comments)
+
+    class Meta:
+        strict = True
+        model = Post
+        sqla_session = db.session
+
+
+simple_post_schema = PostSchema(only=['id', 'title'])
+
+main_post_schema = PostSchema(only=['id', 'title', 'content', 'comments_count', 'created_at', 'user', 'description', 'updated_at'])
+post_list_schema = PostSchema(only=['id', 'title', 'comments_count', 'created_at', 'user'], many=True)
+
+after_create_post_schema = PostSchema(only=['id', 'title', 'content', 'description', 'created_at', 'user'])
+after_update_post_schema = PostSchema(only=['id', 'title', 'content', 'description', 'created_at', 'user'])
+after_delete_post_schema = PostSchema(only=['id', 'title', 'status'])
+
+
+class PostFormSchema(ma.ModelSchema):
+    class Meta:
+        strict = True
+        model = Post
+        sqla_session = db.session
+        fields = ['title', 'content', 'description']
+
+
+before_create_post_schema = PostFormSchema()
+before_update_post_schema = PostFormSchema()
+
+
 
 
 # class PostSchema(ma.ModelSchema):
@@ -38,51 +88,3 @@ from app.schema.board import simple_board_schema
 #     content = fields.String()
 #
 #
-'''
-스키마는 입력값 정제 vs 나가는값 정제
-
-입력값 정제 : 필요한 스키마 만들어서 사용
-출력값 정제 : 기본 스키마에서 only를 사용
-'''
-
-
-class DefaultPostSchema(ma.ModelSchema):
-    class Meta:
-        strict = True
-        model = Post
-        sqla_session = db.session
-
-
-class ExtensionPostSchema(DefaultPostSchema):
-    board = fields.Nested(simple_board_schema)
-    user = fields.Nested(simple_user_schema)
-
-    comments_count = fields.Method('get_comments_count')
-
-    status = EnumField(PostStatus)
-
-    def get_comments_count(self, obj):
-        comments = [c for c in obj.comments if c.status != CommentStatus.DELETED]
-        return len(comments)
-
-
-simple_post_schema = ExtensionPostSchema(only=['id', 'title'])
-
-main_post_schema = ExtensionPostSchema(only=['id', 'title', 'content', 'comments_count', 'created_at', 'user', 'description', 'updated_at'])
-post_list_schema = ExtensionPostSchema(only=['id', 'title', 'comments_count', 'created_at', 'user'])
-
-after_create_post_schema = ExtensionPostSchema(only=['id', 'title', 'content', 'description', 'created_at', 'user'])
-after_update_post_schema = ExtensionPostSchema(only=['id', 'title', 'content', 'description', 'created_at', 'user'])
-after_delete_post_schema = ExtensionPostSchema(only=['id', 'title', 'status'])
-
-
-class PostFormSchema(ma.ModelSchema):
-    class Meta:
-        strict = True
-        model = Post
-        sqla_session = db.session
-        fields = ['title', 'content', 'description']
-
-
-before_create_post_schema = PostFormSchema()
-before_update_post_schema = PostFormSchema()
